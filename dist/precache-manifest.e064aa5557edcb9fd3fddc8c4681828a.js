dcf9a72b171afd1b3bcb8acecb65241fcf4708a1
Azor <i@azor.net>
2021-04-25 22:58:41 +0200

    Add dist

---
+++ dist/precache-manifest.e064aa5557edcb9fd3fddc8c4681828a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "97f38f03584f04864c5a",
+    "url": "/js/chunk-vendors.58b7b7fb.js"
+  },
+  {
+    "revision": "daf6586c8adc14f3434f",
+    "url": "/css/main.75b7033d.css"
+  },
+  {
+    "revision": "daf6586c8adc14f3434f",
+    "url": "/js/main.a649cd10.js"
+  },
+  {
+    "revision": "361524fc7a2ddaaf6c12afee36f512cc",
+    "url": "/index.html"
+  }
+];
